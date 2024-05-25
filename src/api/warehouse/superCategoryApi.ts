86c0c9b285b9ac67a620b770c89c7dd54c56b0ac
Huynh Minh Thuan <hmtminhthuan@gmail.com>
2024-05-26 03:37:35 +0700

    Merge

--- src/api/warehouse/superCategoryApi.ts
+++ src/api/warehouse/superCategoryApi.ts
@@ -1,13 +1,5 @@
 import axiosInstances from "@/config/axios";
 
-<<<<<<< HEAD
-const requestWareHouse = axiosInstances.warehouse
-const ROOT_SUPER_CATEGORY = "/superCategory"
-
-const getAllSuperCategory = () => requestWareHouse.post(`${ROOT_SUPER_CATEGORY}/GetAllSuperCategory`)
-
-const superCategoryApi ={
-=======
 
 const requestWareHouse = axiosInstances.warehouse
 const ROOT_SUPERCATEGORY = "/superCategory"
@@ -15,7 +7,6 @@ const ROOT_SUPERCATEGORY = "/superCategory"
 const getAllSuperCategory = () => requestWareHouse.post(`${ROOT_SUPERCATEGORY}/GetAllSuperCategory`, )
 
 const superCategoryApi = {
->>>>>>> 19a452aa1880d8d6b294a21dcb6a57ce0229d307
     getAllSuperCategory
 };
 
