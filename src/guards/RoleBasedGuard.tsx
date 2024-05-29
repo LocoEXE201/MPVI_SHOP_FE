4c90069b9bcea14e145a78cd87d190bada3721ba
Huynh Minh Thuan <hmtminhthuan@gmail.com>
2024-05-29 22:05:08 +0700

    Update

--- src/guards/RoleBasedGuard.tsx
+++ src/guards/RoleBasedGuard.tsx
@@ -37,7 +37,9 @@ export default function RoleBasedGuard({
   }, [isAuthenticated]);
 
   const navigateToPage = (route: string) => {
-    localStorage.setItem(LOCALSTORAGE_CONSTANTS.CURRENT_PAGE, route);
+    if (typeof window !== "undefined") {
+      localStorage.setItem(LOCALSTORAGE_CONSTANTS.CURRENT_PAGE, route);
+    }
     router.push(route);
   };
 
